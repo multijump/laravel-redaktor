a89101b49fe3f946eb9a926dab798807e0b4e765
David Saavedra <itsdavidsq@gmail.com>
2020-09-15 23:44:08 +0100

    Simplify `InteractsWithApplication` concern
    Rather than implementing our own application bootstrapping process and since we
    require the `laravel\laravel` package as a development dependency, we leverage
    the framework`s bootstrap process.

--- tests/Concerns/InteractsWithApplication.php
+++ tests/Concerns/InteractsWithApplication.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace DSLabs\LaravelRedaktor\Tests\Concerns;
 
-use Illuminate\Config\Repository;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
 
 /**
  * @property-read Application $app
@@ -35,59 +35,17 @@ trait InteractsWithApplication
 
     private function createApplication(): Application
     {
-        $app = new \Illuminate\Foundation\Application(__DIR__ . '/../../vendor/laravel/laravel/');
+        /** @var Application $app */
+        $app = require __DIR__.'/../../vendor/laravel/laravel/bootstrap/app.php';
 
-        $app->singleton(
-            \Illuminate\Contracts\Http\Kernel::class,
-            \App\Http\Kernel::class
-        );
+        $app->instance('request', new Request());
+        $app->make(Kernel::class)->bootstrap();
 
-        $app->singleton(
-            \Illuminate\Contracts\Console\Kernel::class,
-            \App\Console\Kernel::class
-        );
-
-        $app->singleton(
-            \Illuminate\Contracts\Debug\ExceptionHandler::class,
-            \Illuminate\Foundation\Exceptions\Handler::class
-        );
-
-        $app->make(\Illuminate\Foundation\Bootstrap\LoadConfiguration::class)
-            ->bootstrap($app);
-
-        $app->detectEnvironment(static function () {
-            return 'test';
-        });
-
-        $this->loadPackageServiceProviders($app);
-        $this->setupApplication($app);
-        $this->bootstrapServiceProviders($app);
+        foreach ($this->getServiceProviders($app) as $serviceProvider) {
+            $app->register($serviceProvider);
+        }
 
         return $app;
-    }
-
-    private function loadPackageServiceProviders(Application $app): void
-    {
-        /** @var Repository $configRepo */
-        $configRepo = $app->make('config');
-        foreach ($this->getServiceProviders($app) as $packageProvider) {
-            $configRepo->push('app.providers', $packageProvider);
-        }
-    }
-
-    private function setupApplication(Application $app): void
-    {
-        $app->bootstrapWith([
-            \Illuminate\Foundation\Bootstrap\HandleExceptions::class,
-            \Illuminate\Foundation\Bootstrap\RegisterFacades::class,
-            \Illuminate\Foundation\Bootstrap\SetRequestForConsole::class,
-            \Illuminate\Foundation\Bootstrap\RegisterProviders::class,
-        ]);
-    }
-
-    private function bootstrapServiceProviders(Application $app): void
-    {
-        $app->boot();
     }
 
     public function __get($name): Application
